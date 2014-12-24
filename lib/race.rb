cca487f1da854146834342ef1cb7fef9d0fc4d21
kmotorn <kmotorn@gmail.com>
2014-12-24 12:44:39 +0200

    solution

--- lib/race.rb
+++ lib/race.rb
@@ -3,7 +3,12 @@ module RaceBet
 
     class << self
       def score(guesses, winners)
-
+        scores = {0 => 15, 1 => 10, 2 => 5, 3 => 3, 4 => 1}
+        zipped_results = guesses.zip(winners).map { |arr| arr.to_set }
+        guessed = zipped_results.reject { |arr| arr.count>1 }.to_set.flatten.to_a
+        zipped_results.map!.with_index { |arr, i| arr.count==1 ? scores[i] : 0 }
+        guesses.keep_if { |item| winners.include? item }
+        (zipped_results.reduce 0, :+) + (guesses - guessed).count
       end
     end
 
